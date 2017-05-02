ae4101932be7854b27dbcc75a8d114291bfdc7bd
jonas.geiping <jonas.geiping@googlemail.com>
2017-05-02 10:02:15 +0200

    submodule update

--- helpers/LoadImSequenceReal.m
+++ helpers/LoadImSequenceReal.m
@@ -2,22 +2,32 @@ function [imageSequenceInput] = LoadImSequenceReal(datastr,startFrame,numFrames)
 % load  image sequence from folder datastr
 
 
-fileStruct = dir([datastr,filesep,'*.png']);
+fileStruct = [dir([datastr,filesep,'*.png']);dir([datastr,filesep,'*.jpg']);dir([datastr,filesep,'*.tif'])];
 
 if isempty(fileStruct)
     error('invalid location path, data not found');
 end
 
+if nargin < 3
+    numFrames = length(fileStruct);
+end
+if nargin < 2
+    startFrame = 1;
+end
 
 %test image
 imtest = im2double(imread([datastr,filesep,fileStruct(startFrame).name]));
-[xLarge,yLarge,~] = size(imtest);
+[xLarge,yLarge,numColors] = size(imtest);
 
 
     
-imageSequenceInput = zeros(xLarge,yLarge,3,numFrames);
+imageSequenceInput = zeros(xLarge,yLarge,numColors,numFrames);
 for jj = startFrame:numFrames+startFrame-1
-    imageSequenceInput(:,:,:,jj-startFrame+1)  = im2double(imread([datastr,filesep,fileStruct(jj).name]));
+    if numColors == 3
+        imageSequenceInput(:,:,:,jj-startFrame+1)  = im2double(imread([datastr,filesep,fileStruct(jj).name]));
+    else
+        imageSequenceInput(:,:,jj-startFrame+1)  = im2double(imread([datastr,filesep,fileStruct(jj).name]));
+    end
 end
 
 
