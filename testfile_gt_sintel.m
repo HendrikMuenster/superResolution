d493d2ee202fbee18082ea5d77628bf7e75baf7a
jonas.geiping <jonas.geiping@googlemail.com>
2017-05-30 12:49:52 +0200

    sintel test

---
+++ testfile_gt_sintel.m
@@ -0,0 +1,92 @@
+%% MULTIFRAME MOTION COUPLING FOR VIDEO SUPER RESOLUTION
+% 
+% 
+% Be sure to initialize the submodules and to compile them
+% following their instructions
+
+
+clearvars;
+addpath(genpath(cd)); 
+
+
+
+%% Data properties
+datasetName = 'bandage_1';
+
+startFrame = 1;
+numFrames = 13;
+cslice = ceil(numFrames/2);
+
+factor  = 4;             % Magnification factor
+
+
+
+%% ICCV paper data generation process 
+dataFolder = '/windows/DataJonas/ScieboLocalFolder/Data/videos_scenes/';
+[imageSequenceSmall,imageSequenceLarge] = LoadImSequence([dataFolder,filesep,datasetName],startFrame,numFrames,factor,'bicubic');   
+
+%% load gt flow
+flowFolder = '/windows/DataJonas/ScieboLocalFolder/Data/flow_scenes/';
+[ny,nx,nc,nf] = size(imageSequenceSmall);
+v = zeros(ny*factor,nx*factor,nf-1,2);
+for i = 1:numFrames
+    vTmp = readFlowFile([flowFolder,filesep,datasetName,filesep,'frame_',num2str(i,'%04d'),'.flo']);
+    v(:,:,i,1) = vTmp(:,:,2);
+    v(:,:,i,2) = vTmp(:,:,1);
+end
+
+
+%% Construct algorithm object
+% Input: RGB-Time matlab array 
+%mainSuper = MultiframeMotionCoupling(imageSequenceSmall,'flowField',v);
+mainSuper = MultiframeMotionCoupling(imageSequenceSmall);
+
+%% Set variables (these are the standard parameters)
+
+% Procedure
+mainSuper.factor        = factor;              % magnification factor
+mainSuper.verbose       = 1;                   % enable intermediate output, 1 is text, 2 is image
+mainSuper.framework     = 'flexBox';           % Choose framework for super resolution problem
+                                               % Either 'flexBox' or 'flexBox_vector'
+                                               % or 'prost', if installed
+
+% Problem parameters
+mainSuper.alpha         = 0.01;                % regularizer weight
+mainSuper.beta          = 0.2;                 % flow field complexity
+mainSuper.kappa         = 0.25;                % regularization pendulum
+mainSuper.flowDirection = 'forward';           % flow field direction
+
+% Operator details
+mainSuper.interpMethod = 'average';            % Downsampling operator D
+mainSuper.k = fspecial('gaussian',7,sqrt(0.6));% Blur operator B  
+mainSuper.VDSRFlag     = true;                 % Enable VDSR initial guess
+vl_setupnn()
+
+%% Init flow field and solvers
+tic
+mainSuper.init;
+toc
+
+%% Solve super resolution problem
+tic
+mainSuper.run;
+toc
+
+%% Show error margin
+
+outImage = mainSuper.result1(20:end-20,20:end-20,:,ceil(numFrames/2));
+psnrErr = round(psnr(outImage,imageSequenceLarge(20:end-20,20:end-20,:,ceil(numFrames/2))),2);
+ssimErr = round(ssim(outImage,imageSequenceLarge(20:end-20,20:end-20,:,ceil(numFrames/2))),3);
+disp(['PSNR (central patch, central slice): ',num2str(psnrErr),' dB']);
+disp(['SSIM (central patch, central slice): ',num2str(ssimErr),' ']);
+
+
+%% Visualize either central image or full video
+if mainSuper.verbose > 0
+     vid = implay(mainSuper.result1,2);  
+     set(vid.Parent, 'Position',get(0, 'Screensize'));
+else
+    figure(1), imshow(outImage); title(['PSNR: ', num2str(psnrErr)]); axis image
+end
+%% 
+disp('---------------------------------------------------------------------')
